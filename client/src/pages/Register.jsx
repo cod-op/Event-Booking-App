975e0bdf7e2b5662fc82070a6788a513fcc552d3
cod-op <shlokpatel094@gmail.com>
2026-05-17 22:07:06 +0530

    update

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -68,7 +68,7 @@ const Register = () => {
                                 required
                                 className="w-full px-4 py-3 rounded-lg border border-gray-300 focus:ring-2 focus:ring-gray-700 transition shadow-sm"
                                 value={email}
-                                placeholder=''
+                                placeholder='admin email=admin@eventora.com'
                                 onChange={(e) => setEmail(e.target.value)}
                             />
                         </div>
@@ -79,7 +79,7 @@ const Register = () => {
                                 required
                                 className="w-full px-4 py-3 rounded-lg border border-gray-300 focus:ring-2 focus:ring-gray-700 transition shadow-sm"
                                 value={password}
-                                placeholder='for admin password = StrongPassword@123'
+                                placeholder='admin password = StrongPassword@123'
                                 onChange={(e) => setPassword(e.target.value)}
                             />
                         </div>
